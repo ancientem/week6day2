7bdc7a12b873df184d8be053fb1a379b242c16f8
ancientem <eheaton201@gmail.com>
2020-10-22 18:37:55 -0600

    Update css, javascript files, finish week 6 day 2 project

--- action.js
+++ action.js
@@ -1,31 +1,9 @@
-$('#boxContainers').append(`
-    <div id='row'>
-        <p id='lighterboxes'></p>
-        <p></p>
-        <p></p>
-        <p id='lighterboxes'></p>
-    </div>
-    <div id='row'>
-        <p></p>
-        <p id='lighterboxes'></p>
-        <p id='lighterboxes'></p>
-        <p></p>
-    </div>
-    <div id='row'>
-        <p></p>
-        <p id='lighterboxes'></p>
-        <p id=lighterboxes'></p>
-        <p></p>
-    </div>
-    <div id='row'>
-        <p id='lighterboxes'></p>
-        <p></p>
-        <p></p>
-        <p id='lighterboxes'></p>
-    </div>
-`)
 
-$('#boxContainers').on('click', '#lighterboxes', function (){
-
-    $('#lighterboxes').toggleClass('lightBox')
+for(let i = 0; i < 16; i++){
+    $('#boxContainers').append(`
+    <p class='lighterboxes'></p> 
+    `)
+}
+$('#boxContainers').on('click', '.lighterboxes', function (){
+    $(this).toggleClass('lightBox')
 });
